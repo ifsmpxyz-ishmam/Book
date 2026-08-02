88f938ac4c6e5d352f653cd29c5ab0812ac579a1
ifsmpxyz-ishmam <ifsmpxyz@gmail.com>
2026-08-02 22:11:05 +0600

    Implement Airtable data fetching in getBookings function
    Fetch bookings from Airtable and handle errors.

--- netlify/functions/getBookings.js
+++ netlify/functions/getBookings.js
@@ -1,1 +1,19 @@
+exports.handler = async (event,context) => {
+    const token = process.env.AIRTABLE_TOKEN;
+    const baseId = process.env.AIRTABLE_BASE_ID;
+    const table = 'BookingSystem';
 
+    const url = `https://api.airtable.com/v0/${baseId}/${table}`;
+    try{
+        const response = await fetch(url, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+        const data = await response.json();
+        return { statusCode: 200, body: JSON.stringify(data.records) };
+    }
+    catch (error) {
+        return { statusCode: 500, body: JSON.stringify({ error: 'Failed to fetch data from Airtable' }) };
+    }
+};
