3f75f52e098c7eca81f078eedd8aa32a33b5c96f
ifsmpxyz-ishmam <ifsmpxyz@gmail.com>
2026-08-05 22:44:36 +0600

    Implement booking fetch and display functionality
    Fetch bookings from the server and display them in the container.

--- scirpt.js
+++ scirpt.js
@@ -1,1 +1,38 @@
+const container = document.getElementById('booking-container');
+async function FetchBookings(params) {
+    try {
+        const response = await fetch('/.netlify/functions/getBookings');
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        return data;
+    }
+    catch (error) {
+        console.error('Error fetching bookings:', error);
+        return [];
+    } 
+}
+function displayBookings(bookings) {
+    container.innerHTML = '';
+    bookings.forEach(booking => {
+        const div= document.createElement('div');
+        const p = document.createElement('p');
+        const status = document.createElement('span');
+        p.textContent = `Customer-Name: ${booking.fields.CustomerName}, Time: ${booking.fields.Time}, Date: ${booking.fields.Date}`;
+        status.textContent = `Status: ${booking.fields.Status}`;
+        div.classList.add('booking');;
+        div.appendChild(p);
+        div.appendChild(status);
+        container.appendChild(div);
+    });
+}
+FetchBookings().then(bookings => {
+    displayBookings(bookings);
+});
+
+
+
+
+
 
