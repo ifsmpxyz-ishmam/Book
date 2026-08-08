2c2fc4729ea57dd592d32a398e2e6d1f6b7acaa2
ifsmpxyz-ishmam <ifsmpxyz@gmail.com>
2026-08-08 18:09:15 +0600

    Update getBooking.js

--- netlify/functions/getBooking.js
+++ netlify/functions/getBooking.js
@@ -17,27 +17,4 @@ exports.handler = async (event,context) => {
         return { statusCode: 500, body: JSON.stringify({ error: 'Failed to fetch data from Airtable' }) };
     }
 };
-exports.handler = async (event, context) => {
-    const token = process.env.AIRTABLE_TOKEN;
-    const baseId = process.env.BASE_ID;
-    const table = 'BookingSystem';
-        const url = `https://api.airtable.com/v0/${baseId}/${table}`;
-    try {
-        const response = await fetch(url, {
-            method: 'POST',
-            headers: {
-                'Authorization': `Bearer ${token}`, 
-                'Content-Type': 'application/json'
-            },
-            body: event.body
-        });
-        const data = await response.json();
-        return { statusCode: 200, body: JSON.stringify(data) };
-    } catch (error) {
-        console.error('Error adding Booking:', error);
-        return { statusCode: 500, body:  JSON.stringify({ error: 'Failed to add booking' }) };
-    }
-}
-
-
 
